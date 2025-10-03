0329936193f6c7c950896bcd9e2246030c70e2d1
md1frejo <jonas.mb@gmail.com>
2025-10-03 10:40:43 +0200

    from command line

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+ import Top from './components/Top.vue'
+ 
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <Top />
+</template>
+
+<style scoped></style>
---
+++ src/components/Top.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <p class="text-4xl text-green4g-200">tailwind?</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
